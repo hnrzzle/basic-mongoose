bde98bd71c82b2e955ddda812a9368e1b1121d59
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-17 12:56:35 -0700

    file setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Basic Mongoose
+
+**Author**: Henrique Oliveira
+**Version**: 1.0.0
+
+## Overview
+
+A REST API using Mongoose, Express and NodeJs
---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint no-console: off */
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/connect');
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/heroes'
+
+connect(MONGODB_URI);
+
+server.listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
