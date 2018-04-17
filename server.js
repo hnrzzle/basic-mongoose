c72bc4cf18253527bfbca986d4e98e76861fe4a4
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-17 16:27:52 -0700

    more file setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connect = require('./lib/connect');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/heroes'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/heroes';
 
 connect(MONGODB_URI);
 
